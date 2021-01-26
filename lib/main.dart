2928368fa04438bada07af6b779f7c1e4eb7692f
Amedes Júnior <amedes@gmail.com>
2021-01-26 15:31:37 -0300

    Jan, 22 de 2021 -> Primeiro Commit - Flutter aula 53

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:projeto_perguntas/resultado.dart';
+import 'package:projeto_perguntas/questionario.dart';
+
+main() => runApp(PerguntaApp());
+
+class _PerguntaAppState extends State<PerguntaApp> {
+  var _perguntaSelecionada = 0;
+  final _perguntas = const [
+    {
+      'texto': 'Qual a sua cor favorita ?',
+      'respostas': ['Preto', 'Vermelho', 'Verde', 'Branco']
+    },
+    {
+      'texto': 'Qual o seu animal favorito ?',
+      'respostas': ['Coelho', 'Cobra', 'Elefante', 'Leão']
+    },
+    {
+      'texto': 'Qual o seu instrutor favorito ?',
+      'respostas': ['Maria', 'João', 'Leo', 'Pedro']
+    }
+  ];
+
+  void _responder() {
+    if (temPerguntaSelecionada) {
+      setState(() {
+        _perguntaSelecionada++;
+      });
+      print(temPerguntaSelecionada);
+    }
+  }
+
+  bool get temPerguntaSelecionada {
+    return _perguntaSelecionada < _perguntas.length;
+  }
+
+  @override
+  Widget build(BuildContext conext) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Perguntas'),
+          ),
+          body: temPerguntaSelecionada
+              ? Questionario(
+                  perguntas: _perguntas,
+                  perguntaSelecionada: _perguntaSelecionada,
+                  quandoResponder: _responder,
+                )
+              : Resultado()),
+    );
+  }
+}
+
+class PerguntaApp extends StatefulWidget {
+  @override
+  _PerguntaAppState createState() {
+    return new _PerguntaAppState();
+  }
+}
