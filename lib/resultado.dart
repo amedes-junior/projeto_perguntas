57a7c6ec8f27705ea924787ab9b6d1f2600a4c63
Amedes Júnior <amedes@gmail.com>
2021-01-27 17:18:01 -0300

    Projeto Perguntas -> Jan, 27 de 2021 -> Aula 70

--- lib/resultado.dart
+++ lib/resultado.dart
@@ -1,10 +1,42 @@
 import 'package:flutter/material.dart';
 
 class Resultado extends StatelessWidget {
+  final int pontuacao;
+  final void Function() quandoReiniciarQuestionario;
+
+  Resultado(this.pontuacao, this.quandoReiniciarQuestionario);
+
+  String get fraseResultado {
+    String msg = "Sua Pontuação $pontuacao !!!";
+
+    if (pontuacao < 8) {
+      return '$msg Parabéns!';
+    } else if (pontuacao < 12) {
+      return '$msg Você é bom !';
+    } else if (pontuacao < 16) {
+      return '$msg Impressionante !';
+    } else {
+      return '$msg Nível Jedi !!!';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Parabéns !!!', style: TextStyle(fontSize: 28)),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Center(
+          child: Text(fraseResultado, style: TextStyle(fontSize: 20)),
+        ),
+        FlatButton(
+          child: Text(
+            'Reiniciar ?',
+            style: TextStyle(fontSize: 18),
+          ),
+          textColor: Colors.blue,
+          onPressed: quandoReiniciarQuestionario,
+        )
+      ],
     );
   }
 }
